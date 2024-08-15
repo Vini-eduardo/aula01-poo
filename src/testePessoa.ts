83dd6f9937edfa1e0b7249d3bb6c44f990079957
Vinícius Eduardo <viniciuseduardocorreia@hotmail.com>
2024-08-15 11:25:34 -0300

    Aula 1 - criando e testando classe

---
+++ src/testePessoa.ts
@@ -0,0 +1,9 @@
+//IMPORTAR A CLASSE QUE QUERO UTILIZAR
+import { Pessoa } from './Pessoa';
+
+//INSTANCIAR (CRIAR OBJETO)
+const pessoa1: Pessoa = new Pessoa('Felisberto', 25);
+const pessoa2: Pessoa = new Pessoa('Maria', 21);
+
+console.log(pessoa1.aniversario());
+console.log(pessoa2.correr());
